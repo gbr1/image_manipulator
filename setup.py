6c677a575eee8b82694822050f0aff0fc38bd352
gbr1 <giovannididio.bruno@gmail.com>
2022-03-31 19:11:02 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 from setuptools import setup
+from os import path
 
 package_name = 'image_manipulator'
 
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here,'requirements.txt'),encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name=package_name,
-    version='1.0.0',
+    version='1.0.1',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['requirements.txt'])
     ],
-    install_requires=['setuptools'],
+    install_requires=requirements,
     zip_safe=True,
     maintainer='gbr1',
     maintainer_email='giovannididio.bruno@gmail.com',
